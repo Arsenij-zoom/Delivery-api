4c4f6244d9cde5f030448c3c7d76fa5c9efd03b4
arsenij12 <56472584+arsenij12@users.noreply.github.com>
2024-12-04 21:17:17 +0500

    api

---
+++ src/Delivery.Api/Persistance/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Delivery.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Delivery.Api.Persistance;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+        
+    }
+
+    public DbSet<City> Cities { get; set; }
+    public DbSet<Country> Countries { get; set; }
+}
